4d5976b8f13c8bcba0882a18d90fb283893bae9b
aidar <aidar.babanov@nu.edu.kz>
2017-09-16 16:42:26 +0600

    VideoActivity simple (Using https://github.com/afollestad/material-camera api)

--- app/src/main/java/kz/naimi/interview/VideoActivity.java
+++ app/src/main/java/kz/naimi/interview/VideoActivity.java
@@ -2,12 +2,17 @@ package kz.naimi.interview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.afollestad.materialcamera.MaterialCamera;
 
 public class VideoActivity extends AppCompatActivity {
+
+    MaterialCamera materialCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+        materialCamera = new MaterialCamera(this);
+        materialCamera.start(6969);
     }
 }
