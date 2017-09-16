414029318abcef6d1875908f73b2e35755bd9101
aidar <aidar.babanov@nu.edu.kz>
2017-09-16 17:58:06 +0600

    Added InterviewElement, deleted VideoActivity

---
+++ app/src/main/java/kz/naimi/interview/InterviewElement.java
@@ -0,0 +1,22 @@
+package kz.naimi.interview;
+
+public class InterviewElement {
+    private String question;
+    private String videoAnswer;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getVideoAnswer() {
+        return videoAnswer;
+    }
+
+    public void setVideoAnswer(String videoAnswer) {
+        this.videoAnswer = videoAnswer;
+    }
+}
--- app/src/main/java/kz/naimi/interview/QuestionActivity.java
+++ app/src/main/java/kz/naimi/interview/QuestionActivity.java
@@ -6,8 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+import com.afollestad.materialcamera.MaterialCamera;
 
 public class QuestionActivity extends AppCompatActivity {
+
+    private final static int CAMERA_RQ = 6969;
 
     private TextView questionTextView;
     private Button startRecordingButton;
@@ -22,12 +26,29 @@ public class QuestionActivity extends AppCompatActivity {
         startRecordingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startRecording();
+                startCamera();
             }
         });
     }
-    private void startRecording(){
-        Intent intent = new Intent(this, VideoActivity.class);
-        startActivity(intent);
+
+    private void startCamera(){
+        new MaterialCamera(this)
+                .start(CAMERA_RQ);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Received recording or error from MaterialCamera
+        if (requestCode == CAMERA_RQ) {
+
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Saved to: " + data.getDataString(), Toast.LENGTH_LONG).show();
+            } else if(data != null) {
+                Exception e = (Exception) data.getSerializableExtra(MaterialCamera.ERROR_EXTRA);
+                e.printStackTrace();
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
